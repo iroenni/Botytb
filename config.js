d76395a2811a0776b73bcda46a2210f3bbb87d3c
Iro/dev <iroennis2005+40@gmail.com>
2025-11-22 08:53:29 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   // Bot Configuration
-  BOT_TOKEN: process.env.BOT_TOKEN || '8579217430:AAEcglCDYIrH9nqb-aBuz5A9Q_HjuXKAGUQ',
+  BOT_TOKEN: process.env.BOT_TOKEN || '7983103020:AAHhHCYi1zEqXXFJbz2voVqEqHptNprWzs0',
   
   // API Configuration
   API_URL: 'https://yt-dl.officialhectormanuel.workers.dev/',
